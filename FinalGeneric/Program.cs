4723085ececd52f8a47e84cc36eaf559225cc029
Ahmed Ghareeb <a.s.ghareeb@gmail.com>
2021-01-18 19:45:34 -0500

    first commit

---
+++ FinalGeneric/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FinalGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
