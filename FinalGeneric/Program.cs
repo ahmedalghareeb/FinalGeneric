33573d31d97653098837fce08180344d6d77a397
Ahmed Ghareeb <a.s.ghareeb@gmail.com>
2021-01-18 19:47:42 -0500

    second commit

--- FinalGeneric/Program.cs
+++ FinalGeneric/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FinalGeneric
 {
@@ -8,5 +9,40 @@ namespace FinalGeneric
         {
             Console.WriteLine("Hello World!");
         }
+    }
+
+//    Write a generic class that meets the following requirements:
+//1)    That constrains a developer to only instantiate the class based on value types
+//2)    Contains a private generic collection
+//3)    Has a method to add items to the private collection
+//4)    Has a method that will return an item from the list(e.g.item 3 or item 4 in the list)
+//5)    Has a method that returns a sorted collection with the items in descending order
+    class MyGeneric<T> where T : struct 
+    {
+        private List<T> collection;
+        
+        public void add(T t)
+        {
+            collection.Add(t);
+        }
+
+        public T getItemByIndex(int index)
+        {
+            foreach(T t in collection)
+            {
+                if(collection.FindIndex(t) == index)
+                {
+                    return t;
+                }
+            }
+            
+        }
+
+        public ICollection<T> Mysort()
+        {
+            collection.Sort();
+            return collection;
+        }
+
     }
 }
